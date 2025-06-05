4fb5bc8a4620441511e9738f06fb10dc324ec438
Vinicius V. Parizoto <vini.parizoto@live.com>
2025-06-04 21:53:11 -0300

    feat: habilitando cors #feature/master

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,12 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
